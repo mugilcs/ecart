baf82312ae789601ba349ce6285f80349e3f3306
mugil <mugilan.k890@gmail.com>
2018-09-01 02:00:05 -0700

    shopping cart template in angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css', '../css/bootstrap.css', '../css/bootstrap.css',
+   '../css/bootstrap.min.css', '../assets/css/style.css', '../styles-mn.css',
+   '../assets/css/normalize.css', '../assets/css/ideal-image-slider.css', '../themes/default/default.css',
+   '.././slick/slick.css', '.././slick/slick-theme.css']
+})
+export class AppComponent {
+  title = 'ecart';
+}
